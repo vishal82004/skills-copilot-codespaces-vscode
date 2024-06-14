59501e1d1f8e9c569ca340d530ba667392a61c33
vishal82004 <124353945+vishal82004@users.noreply.github.com>
2024-06-14 12:14:27 +0000

    copilot third message

--- comments.js
+++ comments.js
@@ -1,14 +1,57 @@
-// create web server
+//create a web server
+
+//require the express module
 const express = require('express');
+const fs = require('fs');
+//create a new web server
 const app = express();
-const port = 3000;
 
-// define a route handler for the default home page
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(express.json());
+//create a new route
+app.get('/comments', (req, res) => {
+  //read the comments.json file
+  fs.readFile('comments.json', 'utf8', (err, data) => {
+    if (err) {
+      //if there is an error, send a 500 internal server error
+      res.status(500).send('An internal server error occurred');
+    }
+    //send back the data as a JSON object
+    res.json(JSON.parse(data));
+  });
 });
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-  console.log(`Server is running `);
+//create a new route
+app.post('/comments', (req, res) => {
+  //read the comments.json file
+  fs.readFile('comments.json', 'utf8', (err, data) => {
+    if (err) {
+      //if there is an error, send a 500 internal server error
+      res.status(500).send('An internal server error occurred');
+    }
+    //parse the data as a JSON object
+    const comments = JSON.parse(data);
+    //add the new comment to the comments array
+    comments.push(req.body);
+    //write the updated comments array back to the file
+    fs.writeFile('comments.json', JSON.stringify(comments), (err) => {
+      if (err) {
+        //if there is an error, send a 500 internal server error
+        res.status(500).send('An internal server error occurred');
+      }
+      //send a 201 created status code
+      res.status(201).send('Comment added successfully');
+    });
+  });
 });
+//create a new route
+app.get('/comments/:id', (req, res) => {
+  //read the comments.json file
+  fs.readFile('comments.json', 'utf8', (err, data) => {
+    if (err) {
+      //if there is an error, send a 500 internal server error
+      res.status(500).send('An internal server error occurred');
+    }
+    //parse the data as a JSON object
+    const comments = JSON.parse(data);
+    //find the comment with the specified id
+    const comment = comments.find((c) => c.id === parseInt(req.params.id));
+    //if the comment is not found, send a 404 not found status code
